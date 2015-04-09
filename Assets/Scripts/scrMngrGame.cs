35a7b0e6af8fdd0117893b4af740227c7c9fdd9e
Joshua Lamusga <joshlamusga@outlook.com>
2015-04-09 08:41:03 -0700

    Added objMngrGame to handle persistent logic.

---
+++ Assets/Scripts/scrMngrGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A persistent class to execute global code. Ensure that the game object it is attached to
+/// is in the first scene.
+/// </summary>
+public class scrMngrGame : MonoBehaviour {
+	/// <summary>
+	/// Allows the object to persist across scenes.
+	/// </summary>
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+}
